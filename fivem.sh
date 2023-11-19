f73a0b07c4684535dc37bd5a451315a367d5b467
SzekelyTibor2023 <150288871+SzekelyTibor2023@users.noreply.github.com>
2023-11-19 03:08:25 +0100

    Add files via upload

---
+++ fivem.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Felhasználónév bekérése
+read -p "Kérlek, add meg a felhasználónevet: " username
+
+# Jelszó bekérése
+read -s -p "Kérlek, add meg a jelszót: " password
+echo
+
+# Adatbázisnév bekérése
+read -p "Kérlek, add meg az adatbázis nevét: " dbname
+
+# MySQL-be történő belépés
+mysql -u root -p <<EOF
+CREATE DATABASE IF NOT EXISTS $dbname;
+CREATE USER IF NOT EXISTS '$username'@'%' IDENTIFIED BY '$password';
+GRANT ALL PRIVILEGES ON $dbname.* TO '$username'@'%';
+FLUSH PRIVILEGES;
+EXIT
+EOF
+
+echo "Az adatbázis, felhasználó és jogosultságok sikeresen létrehozva."
